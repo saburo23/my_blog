dabf38d14842d11a69118d1456abc35d983a887d
xiandong wang <keneethwang@gmail.com>
2014-09-06 08:19:29 -0400

    typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
 	validates :name, presence: true, length: {maximum:50}
 	validates :email, presence: true
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }.
+    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
     	uniqueness: { case_sensitive: false }
 end
